1fc35bc8821d201e3f38a7f3f7a92f9ac2ace0a1
NCMartin <70169751+ncmartin108@users.noreply.github.com>
2020-12-04 22:03:00 -0700

    To close issue #1
    Closes: #1
    Adds a Circle2Test and the xml file has been updated to run the tests and compile the classes.

--- Circles/src/edu/nmsu/cs/circles/Circle2.java
+++ Circles/src/edu/nmsu/cs/circles/Circle2.java
@@ -3,20 +3,20 @@ package edu.nmsu.cs.circles;
 public class Circle2 extends Circle
 {
 
-	public Circle2(double x, double y, double radius)
-	{
-		super(y, x, radius);
-	}
+ public Circle2(double x, double y, double radius)    {
+  //Error in the source code. The x and y are transposed.
+    super(x, y, radius);
+ }
 
-	public boolean intersects(Circle other)
-	{
-		double d;
-		d = Math.sqrt(Math.pow(center.x - other.center.x, 2) +
-				Math.pow(center.y - other.center.y, 2));
-		if (d < radius)
-			return true;
-		else
-			return false;
-	}
+ public boolean intersects(Circle other)
+ {
+  double d;
+  d = Math.sqrt(Math.pow(center.x - other.center.x, 2) +
+    Math.pow(center.y - other.center.y, 2));
+  if (d < radius)
+   return true;
+  else
+   return false;
+ }
 
 }
---
+++ Circles/src/edu/nmsu/cs/circles/Circle2Test.java
@@ -0,0 +1,56 @@
+package edu.nmsu.cs.circles;
+
+/***
+  * JUnit Class to Test Circle2
+  * @author N. Chumacero-Martin
+  * Modified from Circle1Test.java
+  ***/
+
+import org.junit.*;
+import static org.junit.Assert.assertTrue; //This statment needs to be imported statically
+
+public class Circle2Test    {
+   
+// Data you need for each test case
+   private Circle2 circle2;
+   
+   //
+   // Stuff you want to do before each test case
+   //
+   @Before
+   public void setup()    {
+      System.out.println("\nTest starting...");
+      circle2 = new Circle2(1, 2, 3);
+   }
+   
+   //
+   // Stuff you want to do after each test case
+   //
+   @After
+   public void teardown()    {
+      System.out.println("\nTest finished.");
+   }
+   
+   //
+   // Test a simple positive move
+   //
+   @Test
+   public void simpleMove()    {
+      Point p;
+      System.out.println("Running test simpleMove.");
+      p = circle2.moveBy(1, 1);
+      Assert.assertTrue(p.x == 2 && p.y == 3);
+   }
+   
+   //
+   // Test a simple negative move
+   //
+   @Test
+   public void simpleMoveNeg()    {
+      Point p;
+      System.out.println("Running test simpleMoveNeg.");
+      p = circle2.moveBy(-1, -1);
+      Assert.assertTrue(p.x == 0 && p.y == 1);
+   }
+   
+}
